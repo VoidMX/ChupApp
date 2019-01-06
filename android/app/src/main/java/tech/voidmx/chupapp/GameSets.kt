cb00b98783047826573bbb080f760345979dba4a
Sergio Diosdado <sgdc_98@hotmail.com>
2019-01-05 22:55:55 -0600

    [ANDROID] Main screen and GameSets screen
    [ANDROID] Multiphone menu and joint match menu

---
+++ android/app/src/main/java/tech/voidmx/chupapp/GameSets.kt
@@ -0,0 +1,12 @@
+package tech.voidmx.chupapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GameSets : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_sets)
+    }
+}
