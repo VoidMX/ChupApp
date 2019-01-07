fe9cc9e31a2b25a25b4820023f52992b5a99dfdd
Raul Castellanos <raulcastellanosh@hotmail.com>
2019-01-06 22:09:42 -0600

    [Android] Splash Screen added

---
+++ ios/ChupApp/ChupApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChupApp
+//
+//  Created by Iñaki Janeiro Olague on 12/31/18.
+//  Copyright © 2018 VoidMX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
