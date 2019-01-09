d80b3e0baf605b5624236d17700e112d24755ae9
Iñaki Janeiro Olague <inaki_janeiro@live.com.mx>
2019-01-08 22:45:27 -0600

    [iOS] Adds color pallete programatically in Supporting Files/Colors.swift
    *Adds color palette variables as an extension of the UIColor class
    *Deleted Firebase from the project

---
+++ ios/ChupApp/ChupApp/Supporting Files/Constants/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  ChupApp
+//
+//  Created by Iñaki Janeiro Olague on 1/8/19.
+//  Copyright © 2019 VoidMX. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static let primaryColor = UIColor(named: "Primary")
+    static let primaryColorAlt = UIColor(named: "Primary Alt")
+    static let secondaryColor = UIColor(named: "Secondary")
+    static let secondaryColorAlt = UIColor(named: "Secondary Alt")
+    static let terciaryColor = UIColor(named: "Terciary")
+    static let terciaryColorAlt = UIColor(named: "Terciary Alt")
+    static let actionColor = UIColor(named: "Action")
+    static let actionColorAlt = UIColor(named: "Action Alt")
+
+}
